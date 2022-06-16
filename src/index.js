bed0f7147bb5d5f047e6b3cadda84738660f981a
micgarcia <micgarcia9185@gmail.com>
2022-06-15 17:38:15 -0700

    Initialized files, created item constructor

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './styles.css';
+import Items from './newItems.js';
+
+var content = document.createElement('div');
+document.body.appendChild(content);
+
+/*
+  Project Outline:
+  -Create new .js file that contains constructor function that
+    creates todo items with different properties
+  -Setup html structure of todo app
+    -Should include sidebars for different projects and their
+     todo items, etc
+  -Create separate module(s) that append new todo items to DOM
+  -Create separate module(s) to edit todos, mark them complete, etc
+
+*/
---
+++ src/newItems.js
@@ -0,0 +1,8 @@
+
+export default function Items(title, description, dueDate, priority, notes) {
+  this.title = title;
+  this.description = description;
+  this.dueDate = dueDate;
+  this.priority = priority;
+  this.notes = notes;
+};
